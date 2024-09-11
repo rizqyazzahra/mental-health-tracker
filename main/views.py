dfb46e35097be0eb63d30837bb1ba42d139b0cc7
rizqyazzahra <rizqyazzahraa@gmail.com>
2024-09-11 12:13:03 +0700

    Selesai Tutorial Mengembalikan Data dalam Bentuk XML, JSON

--- main/views.py
+++ main/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.core import serializers
 from django.shortcuts import render, redirect   # Tambahkan import redirect di baris ini
 from main.forms import MoodEntryForm
 from main.models import MoodEntry
@@ -25,3 +27,17 @@ def create_mood_entry(request):
 
     context = {'form': form}
     return render(request, "create_mood_entry.html", context)
+
+def show_xml(request):
+    data = MoodEntry.objects.all()
+
+def show_xml(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json(request):
+    data = MoodEntry.objects.all()
+
+def show_json(request):
+    data = MoodEntry.objects.all()
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
