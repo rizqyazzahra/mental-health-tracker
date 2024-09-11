8fa15edc757247f8be06bccaa51f0c13b6c16c4a
rizqyazzahra <rizqyazzahraa@gmail.com>
2024-09-11 15:41:05 +0700

    Selesai Tutorial: Mengembalikan Data Berdasarkan ID

--- main/views.py
+++ main/views.py
@@ -41,3 +41,17 @@ def show_json(request):
 def show_json(request):
     data = MoodEntry.objects.all()
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def show_xml_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+
+def show_xml_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+
+def show_json_by_id(request, id):
+    data = MoodEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
